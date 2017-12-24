0c71dc3c5c0b65f9a2c113141aaa462b0c0bf13b
apetoman <85765066@qq.com>
2017-12-24 15:13:25 +0800

    完成

--- app/src/main/java/com/example/apetoman/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/apetoman/gitdemo/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.apetoman.gitdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btSub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w("Tag","GitDemo");
+        Log.w("Tag", "GitDemo");
+        btSub = findViewById(R.id.bt_sub);
+        btSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MasterActivity.openmaster(MainActivity.this, null);
+            }
+        });
 
     }
 }
